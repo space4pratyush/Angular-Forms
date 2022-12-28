14d76ac6ae4a2c468f71ff13caf4eef57f2a34c2
space4pratyush <space4pratyush@gmail.com>
2022-12-28 21:56:28 +0530

    Binding data to a model class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,7 @@ export class AppComponent implements OnInit {
   
   topics=['Angular', 'React', 'Vue'];
 
+  userModel = new User('Rob', 'pratyush@gmail.com', 9268223104,'','morning', true);
   constructor() { }
 
   ngOnInit() {
